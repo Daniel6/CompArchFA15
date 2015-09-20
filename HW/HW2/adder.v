8b3d1be821ae41f283bba9f8c864575439ac1e2f
Daniel Bishop <DBishop.rwc@gmail.com>
2015-09-20 14:01:12 -0400

    HW 2
    done

--- HW/HW2/adder.v
+++ HW/HW2/adder.v
@@ -1,3 +1,7 @@
+`define XOR xor #50
+`define AND and #50
+`define OR or #50
+
 module behavioralFullAdder(sum, carryout, a, b, carryin);
 output sum, carryout;
 input a, b, carryin;
@@ -7,15 +11,37 @@ endmodule
 module structuralFullAdder(out, carryout, a, b, carryin);
 output out, carryout;
 input a, b, carryin;
-  // Your adder code here
+wire AxorB, carryout_condition1, carryout_condition2;
+`XOR xor1(AxorB, a, b);
+`AND and1(carryout_condition1, AxorB, carryin); // If only one of a and b is high, and carryin is high, then carryout is high
+`AND and2(carryout_condition2, a, b); // If both a and b are high, then carryout is high
+`OR or1(carryout, carryout_condition1, carryout_condition2);
+`XOR xor2(out, AxorB, carryin); // Sum is high only when only one of a or b is high or both are matching values and carryin is high
 endmodule
 
 module testFullAdder;
 reg a, b, carryin;
-wire sum, carryout;
-behavioralFullAdder adder (sum, carryout, a, b, carryin);
+wire sum, carryout, test_sum, test_carryout;
+behavioralFullAdder model (sum, carryout, a, b, carryin);
+structuralFullAdder test (test_sum, test_carryout, a, b, carryin);
 
 initial begin
-  // Your test code here
+$display("Cin A B | Sum Cout | Expected");
+carryin=0; a=0; b=0; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
+carryin=0; a=1; b=0; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
+carryin=0; a=0; b=1; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
+carryin=0; a=1; b=1; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
+carryin=1; a=0; b=0; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
+carryin=1; a=1; b=0; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
+carryin=1; a=0; b=1; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
+carryin=1; a=1; b=1; #1000
+$display(" %b  %b %b |  %b    %b  | %b  %b ", carryin, a, b, test_sum, test_carryout, sum, carryout);
 end
 endmodule
--- HW/HW2/decoder.v
+++ HW/HW2/decoder.v
@@ -1,3 +1,7 @@
+`define NOR nor #50
+`define AND and #50
+`define NOT not #50
+
 module behavioralDecoder(out0,out1,out2,out3, address0,address1, enable);
 output out0, out1, out2, out3;
 input address0, address1;
@@ -9,33 +13,44 @@ module structuralDecoder(out0,out1,out2,out3, address0,address1, enable);
 output out0, out1, out2, out3;
 input address0, address1;
 input enable;
-  // Your decoder code here
+wire neitherA0norA1, nAddress0, nAddress1, onlyAddress0, onlyAddress1, both;
+`NOR nor1(neitherA0norA1, address0, address1);
+`AND and1(out0, neitherA0norA1, enable);
+`NOT not1 (nAddress0, address0);
+`NOT not2 (nAddress1, address1);
+`AND and2(onlyAddress0, address0, nAddress1);
+`AND and3(onlyAddress1, address1, nAddress0);
+`AND and4(out1, onlyAddress0, enable);
+`AND and5(out2, onlyAddress1, enable);
+`AND and6(both, address0, address1);
+`AND and7(out3, both, enable);
 endmodule
 
 module testDecoder; 
 reg addr0, addr1;
 reg enable;
-wire out0,out1,out2,out3;
+wire out0, out1, out2, out3;
+wire test0, test1, test2, test3;
 behavioralDecoder decoder (out0,out1,out2,out3,addr0,addr1,enable);
-//structuralDecoder decoder (out0,out1,out2,out3,addr0,addr1,enable); // Swap after testing
+structuralDecoder test (test0,test1,test2,test3,addr0,addr1,enable); // Swap after testing
 
 initial begin
 $display("En A0 A1| O0 O1 O2 O3 | Expected Output");
 enable=0;addr0=0;addr1=0; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 enable=0;addr0=1;addr1=0; #1000
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 enable=0;addr0=0;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 enable=0;addr0=1;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 enable=1;addr0=0;addr1=0; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O0 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 enable=1;addr0=1;addr1=0; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O1 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 enable=1;addr0=0;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O2 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 enable=1;addr0=1;addr1=1; #1000 
-$display("%b  %b  %b |  %b  %b  %b  %b | O3 Only", enable, addr0, addr1, out0, out1, out2, out3);
+$display("%b  %b  %b |  %b  %b  %b  %b  | %b  %b  %b  %b", enable, addr0, addr1, test0, test1, test2, test3, out0, out1, out2, out3);
 end
 endmodule
