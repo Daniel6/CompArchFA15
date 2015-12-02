0833c5dbb99b3b997ccc136250f12d5b1dc95222
Thuc Tran <thuc.tran@students.olin.edu>
2015-12-01 21:57:45 -0500

    working through testing

--- FinalProject/src/instruction_memory.t.v
+++ FinalProject/src/instruction_memory.t.v
@@ -1,6 +1,17 @@
-module test_instruction_module()
+module test_instruction_module();
 
+reg clk;
+reg [31:0] ipnutaddress;
+wire [63:0] output_instructions;
 
+	instructionmemory #(1, "one_core_memory.dat") test_one_core(.clk(clk), 
+									.address(inputaddress), 
+									.dataOut(output_instructions));
+	instructionmemory #(.cores(2),
+		                .instructions("two_core_memory.dat"))
+		                 test_two_core(.clk(clk), 
+		                 	            .address(inputaddress), 
+		                 	            .dataOut(output_instructions));
 
 initial begin 
 
@@ -8,6 +19,7 @@ initial begin
 	    $dumpvars(0, test_instruction_module);
 
 	    
+
 	    $finish;
 
 end 
--- FinalProject/src/instructionmemory.v
+++ FinalProject/src/instructionmemory.v
@@ -6,16 +6,16 @@
 
 module instructionmemory 
 #(
-    cores = 1,
-    instructions = "one-core-memory.dat"
+    parameter cores = 1,
+    parameter instructions = "one-core-memory.dat"
 )
 (
     input                       clk,
     input [31:0]                address,
     output reg [32*cores-1:0]   dataOut
-)
+);
 
-    reg [32*cores-1:0] memory [2**32-1:0];
+    reg [32*cores-1:0] memory [2**10-1:0];
 
     initial $readmemh(instructions, memory);
 
