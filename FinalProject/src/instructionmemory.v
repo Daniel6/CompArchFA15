2d0f7201151e622928c71921f138564fe9e8007e
Jordan Van Duyne <jordannn808@gmail.com>
2015-12-01 21:29:56 -0500

    finish paramaterized IM

---
+++ FinalProject/src/instructionmemory.v
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------
+// Instruction Memory
+//   Positive edge triggered
+//   dataOut always has the value mem[address]
+//------------------------------------------------------------------------
+
+module instructionmemory 
+#(
+    cores = 1
+)
+(
+    input                       clk,
+    input [31:0]                address,
+    output reg [32*cores-1:0]   dataOut;
+)
+
+    reg [32*cores-1:0] memory [2**32-1:0];
+
+    // initial $readmemh("data.dat", mem);
+
+    always @(posedge clk) begin
+        dataOut <= memory[address];
+    end
+
+endmodule
