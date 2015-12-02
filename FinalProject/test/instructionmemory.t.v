5fbe9cffea50b882389871d93932ae9157b759df
Thuc Tran <thuc.tran@students.olin.edu>
2015-12-01 22:17:43 -0500

    Push Test

--- FinalProject/test/instructionmemory.t.v
+++ FinalProject/test/instructionmemory.t.v
@@ -2,22 +2,34 @@ module test_instruction_module();
 
 reg clk;
 reg [31:0] inputaddress;
-wire [63:0] output_instructions;
+wire [63:0] two_output_instructions;
 
-	instructionmemory #(1, "one_core_memory.dat") test_one_core(.clk(clk), 
+wire [31:0] one_output_instructions;
+
+	instructionmemory #(1, "text.dat") dut1(.clk(clk), 
 																.address(inputaddress), 
-																.dataOut(output_instructions));
+																.dataOut(one_output_instructions));
 	instructionmemory #(.cores(2),
-		                .instructions("two_core_memory.dat"))
-		                 test_two_core(.clk(clk), 
+		                .instructions("two-core-memory.dat"))
+		                 dut2(.clk(clk), 
 		                 	            .address(inputaddress), 
-		                 	            .dataOut(output_instructions));
+		                 	            .dataOut(two_output_instructions));
+initial clk = 0;
+always #1 clk = !clk;
 
 initial begin 
 
 		$dumpfile("instruction_module.t.vcd");
 	    $dumpvars(0, test_instruction_module);
 
+	    inputaddress = 0;
+	    #4;
+	    inputaddress = 1;
+	    #4;
+	    inputaddress = 2;
+	    #4
+	    inputaddress = 3;
+	    #4
 	    
 
 	    $finish;
