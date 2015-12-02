bab8152dac4fde54913f89ba3d9d5808aee05fd9
Thuc Tran <thuc.tran@students.olin.edu>
2015-12-01 23:06:05 -0500

    Finish DUT testing for instruction memory

--- FinalProject/test/instructionmemory.t.v
+++ FinalProject/test/instructionmemory.t.v
@@ -16,63 +16,142 @@ wire [31:0] one_output_instructions;
 		                 	            .address(inputaddress), 
 		                 	            .dataOut(two_output_instructions));
 initial clk = 0;
+
 always #1 clk = !clk;
 
 initial begin 
 		$dumpfile("instruction_module.t.vcd");
 	    $dumpvars(0, test_instruction_module);
 		dutPass = 1;
-		
-	   	#2
-	   	if(one_output_instructions != 32'h00000000 || two_output_instructions != 64'h0000000000000000)
+		inputaddress = 0;
+		#4
+	   	if(one_output_instructions !== 32'h00000000 || two_output_instructions !== 64'h0000000000000000)
 	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
 	   	end
-	   	#2
-	   	if(one_output_instructions != 32'h11111111 || two_output_instructions != 64'h1111111111111111)
+	   	inputaddress = inputaddress + 1;
+		#2
+	   	if(one_output_instructions !== 32'h11111111 || two_output_instructions !== 64'h1111111111111111)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'h22222222 || two_output_instructions != 64'h2222222222222222)
+	   	if(one_output_instructions !== 32'h22222222 || two_output_instructions !== 64'h2222222222222222)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	    if(one_output_instructions != 32'h33333333 || two_output_instructions != 64'h3333333333333333)
+	    if(one_output_instructions !== 32'h33333333 || two_output_instructions !== 64'h3333333333333333)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'h44444444 || two_output_instructions != 64'h4444444444444444)
+	   	if(one_output_instructions !== 32'h44444444 || two_output_instructions !== 64'h4444444444444444)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'h55555555 || two_output_instructions != 64'h5555555555555555)
+	   	if(one_output_instructions !== 32'h55555555 || two_output_instructions !== 64'h5555555555555555)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'h66666666 || two_output_instructions != 64'h6666666666666666)
+	   	if(one_output_instructions !== 32'h66666666 || two_output_instructions !== 64'h6666666666666666)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	    if(one_output_instructions != 32'h77777777 || two_output_instructions != 64'h7777777777777777)
+	    if(one_output_instructions !== 32'h77777777 || two_output_instructions !== 64'h7777777777777777)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'h88888888 || two_output_instructions != 64'h8888888888888888)
+	   	if(one_output_instructions !== 32'h88888888 || two_output_instructions !== 64'h8888888888888888)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'h99999999 || two_output_instructions != 64'h9999999999999999)
+	   	if(one_output_instructions !== 32'h99999999 || two_output_instructions !== 64'h9999999999999999)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'haaaaaaaa || two_output_instructions != 64'haaaaaaaaaaaaaaaa)
+	   	if(one_output_instructions !== 32'haaaaaaaa || two_output_instructions !== 64'haaaaaaaaaaaaaaaa)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	    if(one_output_instructions != 32'hbbbbbbbb || two_output_instructions != 64'hbbbbbbbbbbbbbbbb)
+	    if(one_output_instructions !== 32'hbbbbbbbb || two_output_instructions !== 64'hbbbbbbbbbbbbbbbb)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'hcccccccc || two_output_instructions != 64'hcccccccccccccccc)
+	   	if(one_output_instructions !== 32'hcccccccc || two_output_instructions !== 64'hcccccccccccccccc)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	    if(one_output_instructions != 32'hdddddddd || two_output_instructions != 64'hdddddddddddddddd)
+	    if(one_output_instructions !== 32'hdddddddd || two_output_instructions !== 64'hdddddddddddddddd)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	   	if(one_output_instructions != 32'heeeeeeee || two_output_instructions != 64'heeeeeeeeeeeeeeee)
+	   	if(one_output_instructions !== 32'heeeeeeee || two_output_instructions !== 64'heeeeeeeeeeeeeeee)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
-	    if(one_output_instructions != 32'hffffffff || two_output_instructions != 64'hffffffffffffffff)
+	    if(one_output_instructions !== 32'hffffffff || two_output_instructions !== 64'hffffffffffffffff)
+	   	begin 
 	   	dutPass =0;
+	   	$display ("Woo FAIL at address %h", inputaddress);
+	   	$display("%h", one_output_instructions);
+	   	end
+	   	inputaddress = inputaddress + 1;
 	   	#2
 
 	   	if(dutPass == 1 )
