cc3231a28c2017a1d6f570b396c8dae3f490aee8
Jordan Van Duyne <jordannn808@gmail.com>
2015-12-01 22:03:42 -0500

    fix typo in inputaddress

--- FinalProject/test/instructionmemory.t.v
+++ FinalProject/test/instructionmemory.t.v
@@ -1,12 +1,12 @@
 module test_instruction_module();
 
 reg clk;
-reg [31:0] ipnutaddress;
+reg [31:0] inputaddress;
 wire [63:0] output_instructions;
 
 	instructionmemory #(1, "one_core_memory.dat") test_one_core(.clk(clk), 
-									.address(inputaddress), 
-									.dataOut(output_instructions));
+																.address(inputaddress), 
+																.dataOut(output_instructions));
 	instructionmemory #(.cores(2),
 		                .instructions("two_core_memory.dat"))
 		                 test_two_core(.clk(clk), 
