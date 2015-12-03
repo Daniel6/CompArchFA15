bc5cf06881cbdcd0c24f966610c9ded57523ff56
Isaac Getto <isaac.getto@gmail.com>
2015-12-03 15:45:18 -0500

    start register file tests

--- FinalProject/src/registerfile.v
+++ FinalProject/src/registerfile.v
@@ -3,13 +3,13 @@ module registerfile
     parameter cores = 1
 )
 (
-  output [cores-1:0] read_data_1 [31:0] ,
+  output [cores-1:0] read_data_1 [31:0],
   output [cores-1:0] read_data_2 [31:0],
   input [cores-1:0] write_data [31:0],
   input [cores-1:0] read_address_1 [4:0],
   input [cores-1:0] read_address_2 [4:0],
   input [cores-1:0] write_address [4:0],
-  input write_enable,
+  input write_enable [cores-1:0],
   input clk
 );
   reg [31:0] registers[31:0];
@@ -25,7 +25,7 @@ module registerfile
         // if write enable flag is given and
         // the register to write to is not
         // the constant $zero, allow it
-        if (write_enable && (write_address[i] > 0)) begin
+        if (write_enable[i] && (write_address[i] > 0)) begin
           registers[write_address[i]] = write_data[i];
         end
       end
--- FinalProject/test/registerfile.t.v
+++ FinalProject/test/registerfile.t.v
@@ -1,23 +1,41 @@
 module testRegisterFile;
-	wire[31:0] ReadData1;		// Data from first register read
-	wire[31:0] ReadData2;		// Data from second register read
-	reg[31:0] WriteData;		// Data to write to register
-	reg[4:0] ReadRegister1;		// Address of first register to read
-	reg[4:0] ReadRegister2;		// Address of second register to read
-	reg[4:0] WriteRegister;		// Address of register to write
-	reg RegWrite;				// Enable writing of register when High
+	wire[31:0] ReadData1_1core;		// Data from first register read
+	wire[31:0] ReadData2_1core;		// Data from second register read
+	reg[31:0] WriteData_1core;		// Data to write to register
+	reg[4:0] ReadRegister1_1core;		// Address of first register to read
+	reg[4:0] ReadRegister2_1core;		// Address of second register to read
+	reg[4:0] WriteRegister_1core;		// Address of register to write
+	reg RegWrite_1core;				// Enable writing of register when High
+
+	wire [4-1:0] ReadData1_4core [31:0];		// Data from first register read
+	wire [4-1:0] ReadData2_4core [31:0];		// Data from second register read
+	reg [4-1:0] WriteData_4core [31:0];		// Data to write to register
+	reg [4-1:0] ReadRegister1_4core [4:0];		// Address of first register to read
+	reg [4-1:0] ReadRegister2_4core [4:0];		// Address of second register to read
+	reg [4-1:0] WriteRegister_4core [4:0];		// Address of register to write
+	reg RegWrite_4core [4-1:0];				// Enable writing of register when High
+
 	reg clk;					// Clock (Positive Edge Triggered)
-	
+
 	// Register File for 1 core
-	registerfile dut1 #(.cores(1)) (.read_data_1(ReadData1),
-									.read_data_2(ReadData2),
-									.write_data(WriteData),
-									.read_address_1(ReadRegister1),
-									.read_address_2(ReadRegister2),
-									.write_address(WriteRegister),
-									.write_enable(RegWrite),
-									.clk(clk)
-	);
+	registerfile dut1 #(.cores(1)) (.read_data_1(ReadData1_1core),
+									.read_data_2(ReadData2_1core),
+									.write_data(WriteData_1core),
+									.read_address_1(ReadRegister1_1core),
+									.read_address_2(ReadRegister2_1core),
+									.write_address(WriteRegister_1core),
+									.write_enable(RegWrite_1core),
+									.clk(clk));
+
+	// Register File for 4 core
+	registerfile dut4 #(.cores(4)) (.read_data_1(ReadData1_4core),
+									.read_data_2(ReadData2_4core),
+									.write_data(WriteData_4core),
+									.read_address_1(ReadRegister1_4core),
+									.read_address_2(ReadRegister2_4core),
+									.write_address(WriteRegister_4core),
+									.write_enable(RegWrite_4core),
+									.clk(clk));
 
 	initial clk = 0;
 	always #1 clk = !clk;
