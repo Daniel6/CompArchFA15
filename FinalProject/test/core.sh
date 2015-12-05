666f9ae33b1efb07c906ee0218b9f6a3dc563750
Isaac Getto <isaac.getto@gmail.com>
2015-12-05 16:56:08 -0500

    update sh

--- FinalProject/test/core.sh
+++ FinalProject/test/core.sh
@@ -1,1 +1,1 @@
-iverilog -g 2005-sv ../src/multiplexer.v ../src/bitSlice.v ../src/fullAdder.v ../src/sltSlice.v ../src/alu.v ../src/core.v core.t.v -o ../bin/core.vvp && vvp ../bin/core.vvp
+iverilog -g 2005-sv -o ../bin/core.vvp ../src/multiplexer.v ../src/bitSlice.v ../src/fullAdder.v ../src/sltSlice.v ../src/alu.v ../src/core.v core.t.v  && vvp ../bin/core.vvp
