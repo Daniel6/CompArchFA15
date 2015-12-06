38366ad3ec1441105a0425d7c5e60bb5765dc81f
Isaac Getto <isaac.getto@gmail.com>
2015-12-06 14:05:46 -0500

    add store assembly

---
+++ FinalProject/asm/store-4-cores.asm
@@ -0,0 +1,13 @@
+# Store four words into memory
+
+# load data memory address into registers
+xori $t0, 0
+xori $t1, 1
+xori $t2, 2
+xori $t3, 3
+
+# store data into memory
+sw $t3, ($t0)
+sw $t2, ($t1)
+sw $t1, ($t2)
+sw $t0, ($t3)
